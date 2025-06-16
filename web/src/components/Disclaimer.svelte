307b82444c5e5dbe93415a44902be3a1779b8d13
Tri Phuong Nguyen <triphuong@falcolabs.org>
2025-06-16 22:46:51 +0700

    fix disclaimer

--- web/src/components/Disclaimer.svelte
+++ web/src/components/Disclaimer.svelte
@@ -1,7 +1,6 @@
 <p class="text-center font-normal">
-    <strong>Dự án đã biết về thông tin Sở GD&ĐT Bắc Ninh cộng sai điểm thi chuyên. <br> Thông tin sẽ được cập nhật khi Sở xác nhận thông tin trên và cộng lại điểm cho thí sinh trên trang tra điểm chính thức.</strong><br>
     <strong>Lưu ý:</strong> thông tin chỉ nhằm mục đích tham khảo. Dự án Pelican không chịu trách
-    nhiệm hay đảm bảo bất kỳ điều gì về thông tin sau đây.<br />
+    nhiệm hay đảm bảo bất kỳ điều gì về thông tin sau đây. <br />Thông tin có thể không chính xác hoàn toàn, hoặc chưa được cập nhật kịp thời.<br />
     Dự án không "hack" hay xâm nhập trái phép cơ sở dữ liệu của Sở, toàn bộ dữ liệu đều được lấy, lần
     lượt từng số báo danh một, bằng trang tra điểm chính thức. <br /> Xem
     <a class="text-blue-500 underline" href="/disclaimer#mientru">Tuyên bố miễn trừ trách nhiệm</a>
