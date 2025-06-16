637ae706ef7f937072e5e678aba2b32b0ef16998
Tri Phuong Nguyen <triphuong@falcolabs.org>
2025-06-16 22:18:25 +0700

    additional disclaimer

--- web/src/components/Disclaimer.svelte
+++ web/src/components/Disclaimer.svelte
@@ -1,9 +1,8 @@
 <p class="text-center font-normal">
-    <strong>Lưu ý:</strong> thông tin chỉ nhằm mục đích tham khảo. Dự án Pelican không chịu
-    trách nhiệm hay đảm bảo bất kỳ điều gì về thông tin sau đây.<br />
-    Xem
-    <a class="text-blue-500 underline" href="/disclaimer#mientru"
-        >Tuyên bố miễn trừ trách nhiệm</a
-    >
+    <strong>Lưu ý:</strong> thông tin chỉ nhằm mục đích tham khảo. Dự án Pelican không chịu trách
+    nhiệm hay đảm bảo bất kỳ điều gì về thông tin sau đây.<br />
+    Dự án không "hack" hay xâm nhập trái phép cơ sở dữ liệu của Sở, toàn bộ dữ liệu đều được lấy, lần
+    lượt từng số báo danh một, bằng trang tra điểm chính thức. <br /> Xem
+    <a class="text-blue-500 underline" href="/disclaimer#mientru">Tuyên bố miễn trừ trách nhiệm</a>
     <br />&nbsp;
 </p>
