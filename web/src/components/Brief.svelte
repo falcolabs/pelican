a2d9c905ba3e6809945005baac0ff57ef470805e
Tri Phuong Nguyen <triphuong@falcolabs.org>
2025-06-16 23:20:45 +0700

    complete disappearance

--- web/src/components/Brief.svelte
+++ web/src/components/Brief.svelte
@@ -38,7 +38,7 @@
             <span class="font-bold" style={"color:" + color + ";"}>{displayname}</span>
             <p
                 class="text-lg font-semibold"
-                style={"color:" + color === "#FCEEE3" ? "#000000" : color + ";"}
+                style={"color:" + displayname == "Chuyên Hàn" ? "#000000" : color + ";"}
             >
                 {slogan}
             </p>
